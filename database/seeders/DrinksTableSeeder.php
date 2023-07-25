e99d8be71e1b36accded9d0dff2c1787464085a5
WChristianPerera <christianperera13@gmail.com>
2023-07-25 22:33:21 +0200

    to settle

--- database/seeders/DrinksTableSeeder.php
+++ database/seeders/DrinksTableSeeder.php
@@ -5,17 +5,17 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class DrinksTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            DrinksTableSeeder::class,
-        ]);
+        foreach (config('drinks') as $drink) {
+            Drink::create($drink);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\DrinkController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('drinks', [DrinkController::class, 'index'])->name('api.drinks.index');
+Route::get('drinks/{drink}', [DrinkController::class, 'show'])->name('api.drinks.show');
+
+// Route for sending emails
+Route::post('leads/', [LeadController::class, 'store'])->name('api.leads.store');
